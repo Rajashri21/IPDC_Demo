dd6614d49943195c8a692f2c49aec6a6bbe0dd5d
Rajashri21 <karadrajashri1@gmail.com>
2025-03-25 18:32:55 +0530

    Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: "export",
   images: {
     unoptimized: true, // Required for static export
+    unoptimized: true, // Required for static export
   },
-  pageExtensions: ["page.tsx", "page.jsx", "page.js", "page.ts"],
 };
 export default nextConfig;
