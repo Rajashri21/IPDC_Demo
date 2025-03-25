12483267f1e2ec71bf42885419bcd259287ce3fe
Rajashri21 <karadrajashri1@gmail.com>
2025-03-25 18:12:44 +0530

    Required changes for Static Exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: "export",
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  pageExtensions: ["page.tsx", "page.jsx", "page.js", "page.ts"],
 };
-
 export default nextConfig;
